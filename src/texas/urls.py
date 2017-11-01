d54c539ae616177b1ce4f19783e0cd5d47d5c3ad
XIAOYICHEN <xiaoyic@cmu.edu>
2017-10-31 23:40:13 -0400

    [update]modify structure

--- src/texas/urls.py
+++ src/texas/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views, views_account
+from . import views, views_account, views_game
 from .forms import *
 
 urlpatterns = [
@@ -26,18 +26,18 @@ urlpatterns = [
     url(r'^add_friend/(?P<user_name>.+)$', views.add_friend, name='add_friend'),
     url(r'^delete_friend/(?P<user_name>.+)$', views.delete_friend, name='delete_friend'),
 
-    url(r'^new_game$', views.new_game, name='new_game'),
-    url(r'^dashboard$', views.dashboard, name='dashboard'),
-    url(r'^myfriends$', views.myfriends, name='myfriends'),
-    url(r'^scoreboard$', views.scoreboard, name='scoreboard'),
+    url(r'^new_game$', views_game.new_game, name='new_game'),
+    url(r'^dashboard$', views_game.dashboard, name='dashboard'),
+    url(r'^myfriends$', views_game.myfriends, name='myfriends'),
+    url(r'^scoreboard$', views_game.scoreboard, name='scoreboard'),
     # should with (?P<user_name>.+)$, but for static page testing I just move it
-    url(r'^search_friend$', views.search_friend, name='search_friend'),
+    url(r'^search_friend$', views_game.search_friend, name='search_friend'),
     # url(r'^get_coupon/(?P<user_name>.+)$', views.get_coupon, name='get_coupon'),
     # should with game id
-    url(r'^game_join$', views.game_join, name='game_join'),
+    url(r'^game_join$', views_game.game_join, name='game_join'),
     #     this url might not be used but for testing, u can choose render?
-    url(r'^game_init_success$', views.game_init_success, name='game_init_success'),
-    url(r'^game_ongoing$', views.game_ongoing, name='game_ongoing'),
-    url(r'^game_result$', views.game_result, name='game_result'),
+    url(r'^game_init_success$', views_game.game_init_success, name='game_init_success'),
+    url(r'^game_ongoing$', views_game.game_ongoing, name='game_ongoing'),
+    url(r'^game_result$', views_game.game_result, name='game_result'),
     url(r'^about$', views.about, name='about'),
 ]
