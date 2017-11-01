ad39a1f9d3bb832d72376493bde06911e7cec4fb
XIAOYICHEN <xiaoyic@cmu.edu>
2017-10-31 23:38:43 -0400

    [update]new game method

---
+++ src/texas/views_game.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from datetime import date
+from django.contrib import auth
+from django.urls import reverse
+
+from texas.forms import *
+from texas.models import *
+
+@login_required(login_url='login')
+def new_game(request):
+    context = {}
+    user = request.user
+    # edit here
+
+    if request.method == 'GET':
+        return render(request, 'new_game.html', context)
+    else:
+        # new a game
+        # validate input ???
+        # entry_funds = request.POST['entry_funds']
+        # no_players = request.POST['no_players']
+        # new_game = Game(creator=request.user, player_num=no_players, entry_funds=entry_funds)
+        # new_game.save()
+        # new_game.players.add(request.user)
+        # new socket here?
+        return render(request, 'game_init_success.html', {"game_no": "123"})
+
+
+
+# @login_required(login_url='login')
+def dashboard(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'dashboard.html', context)
+# @login_required(login_url='login')
+def myfriends(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'myfriends.html', context)
+
+# @login_required(login_url='login')
+def scoreboard(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'scoreboard.html', context)
+
+# @login_required(login_url='login')
+def search_friend(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'search_friend.html', context)
+
+# @login_required(login_url='login')
+def game_join(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'game_join.html', context)
+
+# @login_required(login_url='login')
+def game_init_success(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'game_init_success.html', context)
+
+# @login_required(login_url='login')
+def game_ongoing(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'game_ongoing.html', context)
+
+# @login_required(login_url='login')
+def game_result(request):
+    context = {}
+    user = request.user
+    # edit here
+    return render(request, 'game_result.html', context)
