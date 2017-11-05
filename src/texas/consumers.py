3cc3f6a91dba74935304a3d7cbd873f1ede3f039
minyi <minyi@cmu.edu>
2017-11-05 18:41:29 -0500

    [update]send web socket start new games

--- src/texas/consumers.py
+++ src/texas/consumers.py
@@ -2,9 +2,11 @@ import json
 import logging
 from channels import Group
 from channels.sessions import channel_session
-from texas.models import Game
+from texas.models import *
 from django.contrib.auth.models import User
 from django.core import serializers
+import deuces
+import json
 
 log = logging.getLogger(__name__)
 
@@ -46,6 +48,30 @@ def ws_connect(message):
     data = {"photo_src": str(user.userinfo.profile_photo_src), "username": user.username}
     Group('bet-' + game_no, channel_layer=message.channel_layer).send({"text": json.dumps(data)})
 
+    if game.players.count() == game.player_num:
+        new_game_round = GameRound(game=game)
+        new_game_round.start()
+        new_game_round.save()
+        new_game_dict = {}
+        new_game_dict['message_type'] = "round-update"
+        new_game_dict['round_id'] = new_game_round.id
+
+        dealer_string = str(new_game_round.dealer_cards)
+        dealer_pretty_string = ''
+        for card in dealer_string.split(','):
+            dealer_pretty_string += deuces.Card.int_to_str(int(card)) + ','
+
+        new_game_dict['dealer_cards'] = dealer_pretty_string[:-1]
+
+        player_dict = json.loads(new_game_round.player_cards)
+        for player in player_dict:
+            cards = []
+            for card in player_dict[player]:
+                cards.append(deuces.Card.int_to_str(int(card)))
+            player_dict[player] = cards
+        new_game_dict['player_cards'] = player_dict
+
+        Group('bet-' + game_no, channel_layer=message.channel_layer).send({"text": json.dumps(new_game_dict)})
 
 
 @channel_session
